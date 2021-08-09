7debd67a80f620a599c446dc32937ee66df3afc3
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-08-09 18:33:35 +0200

    add validation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // initialize configService to get data from it
   const configService = app.get(ConfigService);
+
+  // add global validation pipe
+  app.useGlobalPipes(new ValidationPipe());
 
   // get port from configService
   const port = configService.get('port');
--- src/notifications/dto/create-notification.dto.ts
+++ src/notifications/dto/create-notification.dto.ts
@@ -1,11 +1,21 @@
+import { IsString, IsArray, IsIn } from 'class-validator';
+
 export class CreateNotificationDto {
+  @IsString()
+  @IsIn(['single', 'group'])
   type: string;
 
+  @IsString()
   body: string;
 
+  @IsString()
   title: string;
 
+  @IsString()
+  @IsIn(['sms', 'push'])
   delivery_method: string;
 
+  @IsArray()
+  @IsString({ each: true })
   consumers: string[];
 }
