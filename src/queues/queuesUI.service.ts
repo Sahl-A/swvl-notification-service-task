99dc3ecc55e011dff86351d264099b709ec7f2eb
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-08-12 22:51:02 +0200

    add bull-board UI service

--- src/queues/queuesUI.service.ts
+++ src/queues/queuesUI.service.ts
@@ -4,17 +4,24 @@ import { Queue } from 'bull';
 import { createBullBoard } from '@bull-board/api';
 import { BullAdapter } from '@bull-board/api/bullAdapter';
 import { ExpressAdapter } from '@bull-board/express';
+import { jobQueues } from '../shared/enums/jobQueues.enums';
 
 const serverAdapter = new ExpressAdapter();
 
 @Injectable()
 class QueueUIProvider {
   constructor(
-    @InjectQueue('notification') private readonly notificationQueue: Queue,
+    @InjectQueue(jobQueues.SMS)
+    private readonly smsNotificationQueue: Queue,
+    @InjectQueue(jobQueues.PUSH)
+    private readonly pushNotificationQueue: Queue,
   ) {
     /** Add queues with adapter, one-by-one */
     createBullBoard({
-      queues: [new BullAdapter(this.notificationQueue)],
+      queues: [
+        new BullAdapter(this.smsNotificationQueue),
+        new BullAdapter(this.pushNotificationQueue),
+      ],
       serverAdapter: serverAdapter,
     });
 
