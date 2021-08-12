c436a7dda8e460ee612289ad0c94a8f478b21034
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-08-12 22:52:31 +0200

    Add queues processors

---
+++ src/queues/processors/pushNotificationQueue.processor.ts
@@ -0,0 +1,42 @@
+import { Processor, Process, OnQueueCompleted } from '@nestjs/bull';
+import { InjectModel } from '@nestjs/mongoose';
+import { Job } from 'bull';
+import { Model } from 'mongoose';
+import { PushNotificationProvider } from '../../providers/push.service';
+import {
+  Notification,
+  NotificationDocument,
+} from '../../notifications/models/notification.model';
+import { notificationType } from '../../shared/enums/notificationType.enums';
+import { jobQueues } from '../../shared/enums/jobQueues.enums';
+
+@Processor(jobQueues.PUSH)
+export class PushNotificationConsumer {
+  constructor(
+    @InjectModel('Notification')
+    private readonly NotificationModel: Model<Notification>,
+    private readonly pushNotificationProvider: PushNotificationProvider,
+  ) {}
+  @Process()
+  async pushNotificationProcessor(job: Job<NotificationDocument>) {
+    switch (job.data.type) {
+      case notificationType.SINGLE:
+        this.pushNotificationProvider.send(job.data);
+        break;
+
+      case notificationType.GROUP:
+        this.pushNotificationProvider.sendBulk(job.data);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  @OnQueueCompleted()
+  async markNotificationAsSent(job: Job) {
+    const filter = { _id: job.data._id };
+    const update = { sent: true };
+    await this.NotificationModel.findOneAndUpdate(filter, update);
+  }
+}
---
+++ src/queues/processors/smsNotificationQueue.processor.ts
@@ -0,0 +1,42 @@
+import { Processor, Process, OnQueueCompleted } from '@nestjs/bull';
+import { InjectModel } from '@nestjs/mongoose';
+import { Job } from 'bull';
+import { Model } from 'mongoose';
+import { jobQueues } from '../../shared/enums/jobQueues.enums';
+import { notificationType } from '../../shared/enums/notificationType.enums';
+import {
+  Notification,
+  NotificationDocument,
+} from '../../notifications/models/notification.model';
+import { SMSNotificationProvider } from '../../providers/sms.service';
+
+@Processor(jobQueues.SMS)
+export class SmsNotificationConsumer {
+  constructor(
+    @InjectModel('Notification')
+    private readonly NotificationModel: Model<Notification>,
+    private readonly smsNotificationProvider: SMSNotificationProvider,
+  ) {}
+  @Process()
+  async smsNotificationProcessor(job: Job<NotificationDocument>) {
+    switch (job.data.type) {
+      case notificationType.SINGLE:
+        this.smsNotificationProvider.send(job.data);
+        break;
+
+      case notificationType.GROUP:
+        this.smsNotificationProvider.sendBulk(job.data);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  @OnQueueCompleted()
+  async markNotificationAsSent(job: Job) {
+    const filter = { _id: job.data._id };
+    const update = { sent: true };
+    await this.NotificationModel.findOneAndUpdate(filter, update);
+  }
+}
