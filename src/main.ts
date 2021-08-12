c706d02335db9bea84bbebe27244a928a4437870
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-08-13 01:42:40 +0200

    add http-logging interceptor

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { serverAdapter } from './queues/queuesUI.service';
 import * as swaggerUi from 'swagger-ui-express';
 import * as swaggerDocument from '../openAPI/notifications.openAPI.json';
+import * as winston from 'winston';
+import { HttpLoggingInterceptor } from './logging/http-logging.interceptor';
+import { winstonLoggerOptions } from './logging/winston-options';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +17,10 @@ async function bootstrap() {
 
   // add global validation pipe
   app.useGlobalPipes(new ValidationPipe());
+
+  // add logging
+  const logger = winston.createLogger(winstonLoggerOptions);
+  app.useGlobalInterceptors(new HttpLoggingInterceptor(logger));
 
   // get the bull-board ui
   app.use('/admin/queues', serverAdapter.getRouter());
