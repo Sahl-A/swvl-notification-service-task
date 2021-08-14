d28025b928cde413e2d643f2236d09b6529b0459
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-08-14 10:46:03 +0200

    remove uncessary comments

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,10 +1,9 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { QueuesProducerSerive } from '../queues/queues.producer';
 
 @Injectable()
 export class TasksService {
-  private readonly logger = new Logger(TasksService.name);
   constructor(private readonly queuesProducerSerive: QueuesProducerSerive) {}
 
   @Cron('*/1 * * * * *')
